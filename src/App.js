a3a8f7a81b10c193150e5d75360bade69814937c
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2022-01-28 00:52:25 +0500

    add para

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>wel come to mustafa's worlds</p>
+        <p>wel come to mustafa's worlds</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
