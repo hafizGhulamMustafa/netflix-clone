9faad67fda32a3e5f834c1298a96e9b2961b21fc
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2022-01-31 22:10:07 +0500

    add heading file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from './heading';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>wel come to mustafa's worlds</p>
-        <p>wel come to mustafa's worlds</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Heading/>
   );
 }
 
---
+++ src/heading.js
@@ -0,0 +1,7 @@
+
+const Heading =()=>{
+    return(
+    <h1>Hello world</h1>
+    )
+}
+export default Heading;
