b1031d86113ac241fc5535f3822a766f40105805
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2022-06-29 12:36:06 +0500

    git init

---
+++ src/useref.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArrayPrec = () => {
+
+  const arr = [1,3,4,5,6,7];
+  const arr2= 
+  return (
+    <h1> {arr.map((a)=><p>{a.name} {a.id} {a.class}{a.rollNo}x</p>)}</h1>
+  
+  )
+}
+
+export default ArrayPrec;
